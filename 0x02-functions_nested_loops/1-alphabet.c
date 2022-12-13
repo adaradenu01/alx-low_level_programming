b932c943d69b0343e1749ee6e556c4d241391391
Ogungbemi Gbenga <adaradenu01@gmail.com>
2022-12-13 09:09:28 -0800

    still coding

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - print all alphabet in lowercase
+ *
+ * Return: 0
+*/
+
+void print_alphabet(void)
+{
+	char x;
+	
+	for (x = 'a'; x <= 'z'; x++)
+		_putchar(x);
+	_putchar('\n');
+}
