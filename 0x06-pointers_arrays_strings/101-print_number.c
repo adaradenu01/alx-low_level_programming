57c60b0d70e3cec8235ad9ee52480110819a9d39
Ogungbemi Gbenga <adaradenu01@gmail.com>
2022-12-22 12:59:51 -0800

    learning codes

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,18 +1,23 @@
 #include "main.h"
-#include <stdio.h>
+
 /**
  * print_number - print numbers chars
  * @n: integer params
- *
  * Return: 0
-*/
+ */
 
 void print_number(int n)
 {
 	unsigned int n1;
 
 
+
 	n1 = n;
+
+
+
+
+
 	if (n < 0)
 	{
 		_putchar('-');
