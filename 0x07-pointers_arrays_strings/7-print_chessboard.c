a10a24328661706851797078d5a5fe3f7eef058a
Ogungbemi Gbenga <adaradenu01@gmail.com>
2023-01-03 16:12:54 -0800

    pointers

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -9,15 +9,12 @@
 
 void print_chessboard(char (*a)[8])
 {
-	int i, j;
+	int i;
 
 
 	for (i = 0; i < 8; i++)
 	{
-		for (j = 0; j < 8; j++)
-		{
-			_putchar(a[i][j]);
-		}
-		_putchar('\n');
+		_putchar(a[i++]);
 	}
+	_putchar('\n');
 }
