56e0785e5e911152e26d0a6e2860082b2dea2053
Ogungbemi Gbenga <adaradenu01@gmail.com>
2023-01-05 07:00:31 -0800

    repetition

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - print strings in reversal
+ * @s: string value
+ *
+ * Return: 0
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return;
+	}
+	
+	_print_rev_recursion(s + 1);
+	_putchar(*s);
+}
